8a5c8e44e1e2f89e3504e7e47e7ef7cab7e5cf98
shahan <shahan@github.com>
2022-04-20 11:03:10 +0500

    Updated code

--- lib/todo/tasks.ex
+++ lib/todo/tasks.ex
@@ -8,15 +8,8 @@ defmodule Todo.Tasks do
 
   alias Todo.Tasks.Task
 
-  @doc """
-  Returns the list of tasks.
 
-  ## Examples
 
-      iex> list_tasks()
-      [%Task{}, ...]
-
-  """
   def list_tasks do
     Repo.all(Task)
   end
@@ -27,83 +20,56 @@ defmodule Todo.Tasks do
   end
 
 
-  @doc """
-  Gets a single task.
 
-  Raises `Ecto.NoResultsError` if the Task does not exist.
 
-  ## Examples
-
-      iex> get_task!(123)
-      %Task{}
-
-      iex> get_task!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_task!(id), do: Repo.get!(Task, id)
 
-  @doc """
-  Creates a task.
 
-  ## Examples
 
-      iex> create_task(%{field: value})
-      {:ok, %Task{}}
+  def get_task_by_project!(id,project_id), do: Repo.get!(Task, id, project_id)
 
-      iex> create_task(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
 
-  """
+
+
+
+  def get_tasks_id_by_project_id(id,project_id) do
+    query = from t in Task, where: t.id == ^id  and t.project_id == ^project_id
+
+    # IO.inspect(query)
+    #  task = %{:select =>  [query]}
+    # |> Map.get(:select)
+    # |> Enum.map(fn(x) -> String.to_atom(x) end)
+    Repo.one(query)
+  end
+
+
   def create_task(attrs \\ %{}) do
     %Task{}
     |> Task.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a task.
 
-  ## Examples
+  def updates_task(attrs \\ %{}) do
+    %Task{}
+    |> Task.changeset(attrs)
+    |> Repo.insert()
+  end
 
-      iex> update_task(task, %{field: new_value})
-      {:ok, %Task{}}
 
-      iex> update_task(task, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
 
-  """
   def update_task(%Task{} = task, attrs) do
     task
     |> Task.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a task.
 
-  ## Examples
-
-      iex> delete_task(task)
-      {:ok, %Task{}}
-
-      iex> delete_task(task)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_task(%Task{} = task) do
     Repo.delete(task)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking task changes.
 
-  ## Examples
-
-      iex> change_task(task)
-      %Ecto.Changeset{data: %Task{}}
-
-  """
   def change_task(%Task{} = task, attrs \\ %{}) do
     Task.changeset(task, attrs)
   end
--- lib/todo_web/controllers/task_controller.ex
+++ lib/todo_web/controllers/task_controller.ex
@@ -6,34 +6,73 @@ defmodule TodoWeb.TaskController do
 
   def index(conn, %{"project_id" => project_id}) do
     tasks = Tasks.list_tasks_by_project_id(project_id)
-    IO.inspect(tasks)
-    render(conn, "index.html", [tasks: tasks, project_id: project_id])
+
+    render(conn, "index.html", tasks: tasks, project_id: project_id)
   end
 
-  def new(conn, %{"project_id" => project_id } = params) do
+  def new(conn, %{"project_id" => project_id} = params) do
     changeset = Task.changeset(%Task{}, params)
-    IO.inspect(changeset)
-    render(conn, "new.html", [changeset: changeset, project_id: project_id] )
+    render(conn, "new.html", changeset: changeset, project_id: project_id)
   end
 
   @spec create(Plug.Conn.t(), %{optional(:__struct__) => none, optional(atom | binary) => any}) ::
           Plug.Conn.t()
-  def create(conn, %{"project_id" => project_id}  = params) do
-    IO.inspect(params)
-    case Tasks.create_task(params) do
+  def create(conn, %{"project_id" => project_id} = params) do
+    task_params = Map.get(params, "task", %{}) |> Map.put("project_id", project_id)
+
+    case Tasks.create_task(task_params) do
       {:ok, task} ->
         conn
         |> put_flash(:info, "Task created successfully.")
-        |> redirect(to: Routes.project_task_path(conn, :show, task))
-
+        |> redirect(to: Routes.project_task_path(conn, :show, task.project_id, task.id))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset, project_id: project_id)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id, "project_id" => project_id}) do
+    task = Tasks.get_tasks_id_by_project_id(id, project_id)
+
+    IO.inspect(task)
+
+    # task = %{:select =>  [task]}
+    # |> Map.get(:select)
+    # |> Enum.map(fn(x) -> String.to_atom(x) end)
+
+    render(conn, "show.html", task: task, project_id: project_id)
+  end
+
+  @spec edit(Plug.Conn.t(), map) :: Plug.Conn.t()
+  def edit(conn, %{"id" => id, "project_id" => project_id}) do
+    task = Tasks.get_tasks_id_by_project_id(id, project_id)
+    IO.inspect(task)
+    changeset = Tasks.change_task(task)
+    # IO.inspect(changeset, label: "shah")
+    render(conn, "edit.html", task: task, changeset: changeset, project_id: project_id)
+  end
+
+  def update(conn, %{"id" => id, "project_id" => project_id} = params) do
+    # task_params = Map.get(params, "task", %{}) |> Map.put("project_id", project_id)
+    IO.inspect(params)
+
+    case Tasks.update_task(params) do
+      {:ok, task} ->
+        conn
+        |> put_flash(:info, "Task updated successfully.")
+        |> redirect(to: Routes.project_task_path(conn, :show, task.project_id, task.id))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", changeset: changeset, project_id: project_id)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
     task = Tasks.get_task!(id)
-    render(conn, "show.html", task: task)
+    {:ok, _task} = Tasks.delete_task(task)
+
+    conn
+    |> put_flash(:info, "Task deleted successfully.")
+    |> redirect(to: Routes.project_task_path(conn, :index, task.project_id))
   end
 end
